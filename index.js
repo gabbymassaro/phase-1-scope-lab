24f51aa2947742a1f70cd192ae2e89095e0db883
Gabriella Massaro <gmassaro807@gmail.com>
2024-05-07 15:33:11 -0400

    Create functions to change a global variable and one const

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+customerName = 'bob'; 
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+    return customerName;
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob'; 
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'someone other than bob';
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'barbara'; 
+}
